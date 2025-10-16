f3b062c278bbd7c16bde28aa8453d02ba45928ba
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-10-16 10:56:28 +0530

    updated v1.js

--- routes/v1.js
+++ routes/v1.js
@@ -6,6 +6,7 @@ const router = express.Router();
 // const exampleRouter = require("./example.route");
 const userRouter = require("./user.route");
 const genderRouter = require("./gender.route");
+const myTargetRouter = require("./mytarget.route");
 
 // Health Check Route
 router.get("/health", (req, res) => {
@@ -17,6 +18,7 @@ router.get("/health", (req, res) => {
 // router.use("/example", exampleRouter);
 router.use("/user", userRouter);
 router.use("/gender", genderRouter);
+router.use("/mytarget", myTargetRouter);
 
 
 
